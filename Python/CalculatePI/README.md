1d86f6abf5015be3346bac159f1f8a457ef71a25
astroxii <67867895+astroxii@users.noreply.github.com>
2022-05-30 17:51:33 -0300

    Add python script

---
+++ Python/CalculatePI/README.md
@@ -0,0 +1,3 @@
+# Calculate PI
+
+This python script is the beginnig of a PI calculation algorithm. Adding a function that auto-creates vectors and sort them properly may make this precise.
---
+++ Python/CalculatePI/calculate_pi.py
@@ -0,0 +1,22 @@
+import math
+
+
+def get_distance(v1: tuple, v2: tuple) -> float:
+    a = (abs(v1[0])+abs(v2[0]))
+    b = (abs(v1[1])+abs(v2[1]))
+
+    return math.sqrt(math.pow(a, 2) + math.pow(b, 2))
+
+
+vectors = [(3, 0), (-3, 0), (0, 3), (0, -3)]
+approx_pi = 0
+
+for i in range(len(vectors)):
+    if i+1 < len(vectors):
+        approx_pi += get_distance(vectors[i], vectors[i+1])
+    else:
+        approx_pi += get_distance(vectors[i], vectors[0])
+
+approx_pi /= 6 #  6 is the absolute distance in the X and Y axis for two vectors.
+
+print(approx_pi)
